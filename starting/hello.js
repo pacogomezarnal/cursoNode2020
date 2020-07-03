df752d4273abcc4d0ced63430c2d58ee938a74c9
Paco Gomez <p.gomez@geekshubs.com>
2020-07-03 08:46:26 +0200

    Asincrono 2

--- starting/hello.js
+++ starting/hello.js
@@ -1,8 +1,26 @@
 const fs = require('fs');
 
+//Sincrona
 const documento=fs.readFileSync('documento.txt','utf8');
 
 console.log(documento);
+console.log("Archivo leido Sincrono");
 
+//Asincrona
+const documento2=fs.readFile('documento.txt','utf8',(err,data)=>{
+    if(err) {
+        console.log("Ha habido un error");
+        
+    }else{
+        console.log(data);
+    }
+    
+}
+)
+console.log("Archivo leido Asincrona");
+
+
+
+/*
 const data=documento+" Paco";
-fs.writeFileSync('documento.txt',data);
+fs.writeFileSync('documento.txt',data);*/
